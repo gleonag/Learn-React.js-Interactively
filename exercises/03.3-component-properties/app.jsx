e23a44cd380133c58238d8773734bc3f3b8b8354
gleonag <gleonag@gmail.com>
2022-10-13 10:38:25 +0000

    exercises w toni

--- exercises/03.3-component-properties/app.jsx
+++ exercises/03.3-component-properties/app.jsx
@@ -18,25 +18,25 @@ const BootstrapCard = (props) => {
 	);
 };
 
+	// 2) add here the new properties into the proptypes object
 BootstrapCard.propTypes = {
 	title: PropType.string,
-	// 2) add here the new properties into the proptypes object
 	imageUrl: PropType.string,
 	description: PropType.string,
 	buttonUrl: PropType.string,
-	buttonLabel: PropType.string,
+	buttonLabel: PropType.string
 };
 
 // 3) Use ReactDOM to add the component into then DOM element #myDiv
 ReactDOM.render =
 	((
 		<BootstrapCard
-			title="Bob Dylan"
+		title="Bob Dylan"
 			imageUrl="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
 			description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
 			buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
 			buttonLabel="Go to wikipedia"
+			
 		/>
 	),
 	document.querySelector("#myDiv"));
-
